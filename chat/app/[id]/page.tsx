9e01448c7828580aafd88c5cb0ed5d2b77c412b7
aneeshpatne <aneeshpatne@gmail.com>
2025-04-16 13:50:22 +0530

    feat: add initial Page component with NavBar and Chat integration

---
+++ chat/app/[id]/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from "@/components/navbar";
+import Chat from "@/components/chat";
+export default function Page() {
+  return (
+    <div className="flex h-screen">
+      <NavBar />
+      <Chat />
+    </div>
+  );
+}
