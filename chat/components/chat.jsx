5c2216831a5aad8f84b4fc8d2936fe1ed6ee587a
aneeshpatne <aneeshpatne@gmail.com>
2025-04-19 13:34:14 +0530

    feat: update SentMessage component to allow whitespace preservation in message display

--- chat/components/chat.jsx
+++ chat/components/chat.jsx
@@ -131,7 +131,7 @@ const SentMessage = React.memo(function SentMessage({ message }) {
   return (
     <div className="flex justify-end w-full">
       <div
-        className="max-w-[60%] px-4 py-2 bg-stone-700 rounded-2xl rounded-tr-none text-white shadow-sm prose prose-invert"
+        className="max-w-[60%] px-4 py-2 bg-stone-700 rounded-2xl rounded-tr-none text-white shadow-sm prose prose-invert whitespace-pre-wrap"
         dangerouslySetInnerHTML={{ __html: micromark(message) }}
       />
     </div>
