b22e282e4e55a24f112b070abc82c6ed10c63ece
aneeshpatne <aneeshpatne@gmail.com>
2025-04-20 17:19:30 +0530

    feat: integrate OpenRouter SDK for enhanced model support in chat API

--- chat/app/api/chat/route.ts
+++ chat/app/api/chat/route.ts
@@ -1,20 +1,32 @@
 // /app/api/chat/route.ts
 import { openai } from "@ai-sdk/openai";
+import { createOpenRouter } from "@openrouter/ai-sdk-provider";
 import { streamText } from "ai";
 import { errorHandler } from "./errorhandler";
 export const runtime = "edge";
 export const maxDuration = 30;
 
+const openrouter = createOpenRouter({
+  apiKey: process.env.OPENROUTER_API_KEY,
+});
+
 export async function POST(req: Request) {
   const { messages, data, provider } = await req.json();
-  console.log("Model from body data:", data?.model);
   const modelId = data?.model || "gpt-4.1-nano"; // Default to gpt-4.1 if no model is provided
-
-  const result = await streamText({
-    model: openai(modelId),
-    temperature: 1,
-    messages,
-  });
+  let result;
+  if (provider == "openai") {
+    result = await streamText({
+      model: openai(modelId),
+      temperature: 1,
+      messages,
+    });
+  } else {
+    result = await streamText({
+      model: openrouter(modelId),
+      temperature: 1,
+      messages,
+    });
+  }
 
   const stream = result.toDataStream();
 
