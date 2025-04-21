d5d44234067b30ebd424d3e0f52fee20fc65f620
aneeshpatne <aneeshpatne@gmail.com>
2025-04-21 16:21:09 +0530

    refactor: remove unused loading component and comment out script tag in layout

--- chat/app/layout.tsx
+++ chat/app/layout.tsx
@@ -25,10 +25,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <script
+        {/* <script
           crossOrigin="anonymous"
           src="//unpkg.com/react-scan/dist/auto.global.js"
-        />
+        /> */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
