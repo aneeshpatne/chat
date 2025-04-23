54a954f368285209b16f9532510d201048e6608a
aneeshpatne <aneeshpatne@gmail.com>
2025-04-23 09:49:20 +0530

    fix: simplify Home component to return a placeholder div

--- chat/app/page.tsx
+++ chat/app/page.tsx
@@ -1,10 +1,5 @@
 import NavBar from "@/components/navbar";
 import Chat from "@/components/initalChat";
 export default function Home() {
-  return (
-    <div className="flex h-screen">
-      <NavBar />
-      <Chat />
-    </div>
-  );
+  return <div>wait</div>;
 }
