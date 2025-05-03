8cf70f0eec1af51292e009252cc5aac7db98d64f
aneeshpatne <aneeshpatne@gmail.com>
2025-05-03 18:14:54 +0530

    feat: Add support for GFM in MemoizedMarkdownBlock component

--- chat/components/MemoizedMarkdownBlock.jsx
+++ chat/components/MemoizedMarkdownBlock.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import { mdComponents } from "./mdcomponents.jsx";
-
+import remarkGfm from "remark-gfm";
 const MemoizedMarkdownBlock = React.memo(
   function MemoizedMarkdownBlock({ content }) {
-    return <ReactMarkdown components={mdComponents}>{content}</ReactMarkdown>;
+    return (
+      <ReactMarkdown remarkPlugins={[remarkGfm]} components={mdComponents}>
+        {content}
+      </ReactMarkdown>
+    );
   },
-  // only rerender if content actually changes
   (prev, next) => prev.content === next.content
 );
 
