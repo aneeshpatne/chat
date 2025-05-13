442457d9426fd63a815af76d23d8d220262d01f9
aneeshpatne <aneeshpatne@gmail.com>
2025-05-13 12:49:54 +0530

    feat: add logging for message fetching by chat ID in getMessagesByChatId function

--- chat/app/actions/fetchmessage.ts
+++ chat/app/actions/fetchmessage.ts
@@ -1,9 +1,10 @@
-// app/actions/getMessages.ts
+"use server";
 import { db } from "@/lib/db";
 import { chatMessages } from "@/lib/schema";
 import { eq } from "drizzle-orm";
 
 export async function getMessagesByChatId(chatId: string) {
+  console.log("Fetching messages for chatId:", chatId);
   return db
     .select()
     .from(chatMessages)
